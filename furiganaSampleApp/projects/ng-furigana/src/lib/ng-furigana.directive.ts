d6a695fea4c7ebad1d00c66fa96b1cbf0c6c9485
Carbammato <demetrio.marino1985@gmail.com>
2019-10-24 20:46:19 +0200

    Do not activate ngFurigana in HTML elements different of <span> and with children (#4)
    * sample app style improvements
    
    * checking for valid elment in order to activate directive #2
    
    * update package.json

--- furiganaSampleApp/projects/ng-furigana/src/lib/ng-furigana.directive.ts
+++ furiganaSampleApp/projects/ng-furigana/src/lib/ng-furigana.directive.ts
@@ -6,6 +6,8 @@ import {
   AfterViewInit
 } from '@angular/core';
 
+const SPAN = 'span';
+
 @Directive({
   selector: '[ngFurigana]'
 })
@@ -15,10 +17,28 @@ export class NgFuriganaDirective implements AfterViewInit {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
+    if (!this.isValidElement()) {
+      console.warn(
+        'Warning: ngFurigana works only on <span> elements without children.'
+      );
+      return;
+    }
+
     this.renderer.setProperty(
       this.el.nativeElement,
       'innerHTML',
       `<ruby>${this.el.nativeElement.innerHTML}<rp>(</rp><rt>${this.ngFurigana}</rt><rp>)</rp></ruby>`
+    );
+  }
+
+  /**
+   * Check if the element passed as argument is a <span> element and has no child tags
+   * @param el the Element to evaluate
+   */
+  isValidElement(): boolean {
+    return (
+      this.el.nativeElement.nodeName.toLowerCase() === SPAN &&
+      this.el.nativeElement.childElementCount.length === 0
     );
   }
 }
